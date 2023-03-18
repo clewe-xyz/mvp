b1feee9ad6dc057a4d67e1fdba4dcd6afd3f3296
skgm <a.bogdanov@andersenlab.com>
2023-03-18 19:43:53 +0400

    fix fill-db endpoint

--- app/api/endpoints/example.py
+++ app/api/endpoints/example.py
@@ -13,6 +13,9 @@ router = APIRouter()
 
 fake = Faker()
 
+IMAGE_NAMES = ['completedq-l.png', 'gempost-b.png']
+QUEST_NAMES = ['Crypto Wallet', 'Dex', 'DAO']
+IMAGE_NAMES = ['completedq-l.png', 'gempost-b.png']
 
 class Msg(BaseModel):
     msg: str
@@ -27,55 +30,59 @@ def test() -> Msg:
 
 @router.post('/fill-db', status_code=http_status.HTTP_200_OK)
 def fill_db(db: Session = Depends(deps.get_db)):
-    users = [
-        models.UserTable(
-            nickname=fake.name(),
-            wallet_address=fake.word(),
-            level=random.randint(1, 5),
-            level_total_exp=random.randint(1, 5),
-            exp_to_next_level=random.randint(1, 5),
-        ) for _ in range(10)
-    ]
-    print('\n\n\n', users)
-    trophies = [
-        models.Trophy(
-            user_id=users[i].id,
-            tx_hash=f'fake hash: {i}',
-            img_url=fake.url(),
-            description=fake.word(),
-        ) for i in range(len(users))
-    ]
 
-    skills = [
-        models.Skill(
-            user_id=users[i].id,
-            topic=fake.word(),
-            title=fake.word(),
-            level=random.randint(1, 5),
-            experience=random.randint(1, 5),
-        ) for i in range(len(users))
-    ]
-
-    quests = [
-        models.Quest(
-            name=fake.word(),
-            slug=f'slug: {i}',
-            topic=fake.word(),
-            skill_reward=random.randint(1, 5),
-            description=fake.word(),
-            difficulty=random.randint(1, 5),
-            exp_reward=random.randint(1, 5),
-        ) for i in range(len(users))
-    ]
     try:
+        users = [
+            models.UserTable(
+                nickname=fake.name(),
+                wallet_address=fake.word(),
+                level=random.randint(1, 5),
+                level_total_exp=random.randint(1, 5),
+                exp_to_next_level=random.randint(1, 5),
+            ) for _ in range(3)
+        ]
+        quests = [
+            models.Quest(
+                name=fake.word(),
+                slug=f'slug: {i}',
+                topic=QUEST_NAMES[i],
+                skill_reward=random.randint(1, 5),
+                description=fake.word(),
+                difficulty=random.randint(1, 5),
+                exp_reward=random.randint(1, 5),
+            ) for i in range(len(users))
+        ]
+
         db.bulk_save_objects(objects=users)
-        db.bulk_save_objects(objects=trophies)
-        db.bulk_save_objects(objects=skills)
         db.bulk_save_objects(objects=quests)
+
         users = db.query(models.UserTable).all()
         quests = db.query(models.Quest).all()
+
         for i in range(len(users)):
             users[i].completed_quests.append(quests[i])
+
+        trophies = [
+            models.Trophy(
+                user_id=users[i].id,
+                tx_hash=f'fake hash: {i}',
+                img_url=IMAGE_NAMES[random.randint(0, 1)],
+                description=fake.word(),
+            ) for i in range(len(users))
+        ]
+
+        skills = [
+            models.Skill(
+                user_id=users[i].id,
+                topic=QUEST_NAMES[i].lower(),
+                title=fake.word(),
+                level=random.randint(1, 5),
+                experience=random.randint(1, 100),
+            ) for i in range(len(users))
+        ]
+
+        db.bulk_save_objects(objects=trophies)
+        db.bulk_save_objects(objects=skills)
         db.commit()
         print(f"All tables are filled")
         return Msg(msg='All tables have been filled!')
