cf3a6bea867da47313f2a80f444db1613b7935e9
skgm <a.bogdanov@andersenlab.com>
2023-03-18 17:38:57 +0400

    add trophy endpoint

--- app/api/endpoints/trophies.py
+++ app/api/endpoints/trophies.py
@@ -10,6 +10,11 @@ from app.schemas import trophies
 router = APIRouter()
 
 
+@router.get('/', response_model=list[trophies.Trophy], status_code=http_status.HTTP_200_OK)
+def get_all_trophies(db: Session = Depends(deps.get_db)):
+    return db.query(Trophy).all()
+
+
 @router.put('/{id}/', response_model=trophies.Trophy, status_code=http_status.HTTP_200_OK)
 def set_trophy_hash(id: int, tx_hash: str, db: Session = Depends(deps.get_db)):
     trophy = db.query(Trophy).filter(Trophy.id == id).first()
