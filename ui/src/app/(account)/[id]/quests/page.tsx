1aeab9fce1c2ce5c9d545f0d7e085664b3bafbfe
Artem Lyubchuk <artemlyubchuk@mail.ru>
2023-03-19 07:42:09 +0300

    Make page always dynamic for demo purpose

--- ui/src/app/(account)/[id]/quests/page.tsx
+++ ui/src/app/(account)/[id]/quests/page.tsx
@@ -8,6 +8,8 @@ import Link from "next/link";
 import { QuestDTO, UserDTO } from "../model";
 import { ProgressLine } from "../../ProgressLine";
 
+export const revalidate = 0;
+
 type Quest = QuestDTO & {
   completed: boolean;
 };
