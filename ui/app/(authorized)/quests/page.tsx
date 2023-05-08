0608e910392eae812daf13a2fd98fb4eeb1d9462
needpower <artemlyubchuk@mail.ru>
2023-05-08 22:03:29 +0300

    Page with list of mocked quests
    Additional features:
    - Mobile navigation

---
+++ ui/app/(authorized)/quests/page.tsx
@@ -0,0 +1,143 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import quest1 from "@/images/quests/01.jpg";
+import quest2 from "@/images/quests/02.jpg";
+import quest3 from "@/images/quests/03.jpg";
+import finance from "@/images/skills/finance.png";
+import token from "@/images/skills/tokens.png";
+import daos from "@/images/skills/daos.png";
+import blockchain from "@/images/skills/blockchain.png";
+import classNames from "classnames";
+
+export default function Quests() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.quest}>
+        <div className={styles.bgImageWrapper}>
+          <Image
+            className={styles.questImage}
+            src={quest1}
+            alt="Finance fundamentals"
+          />
+        </div>
+        <div className={styles.questAbout}>
+          <h3>Finance fundamentals</h3>
+          <p>Dscription of this quest</p>
+          <div className={styles.skills}>
+            <div className={styles.singleSkill}>
+              <Image
+                className={styles.skillIcon}
+                src={finance}
+                alt="Finance skill"
+                width="32"
+              />
+              Finance
+            </div>
+            <div className={styles.singleSkill}>
+              <Image
+                className={styles.skillIcon}
+                src={token}
+                alt="Tokens skill"
+                width="32"
+              />
+              Cpypto basics
+            </div>
+          </div>
+        </div>
+        <div className={styles.questActions}>
+          <a
+            href="#"
+            className={classNames(
+              "button",
+              "button-accent",
+              styles.questActionButton
+            )}
+          >
+            Start quest
+          </a>
+        </div>
+      </div>
+      <div className={styles.quest}>
+        <div className={styles.bgImageWrapper}>
+          <Image
+            className={styles.questImage}
+            src={quest2}
+            alt="Blockchain basics"
+          />
+        </div>
+        <div className={styles.questAbout}>
+          <h3>Blockchain basics</h3>
+          <p>Dscription of this quest</p>
+          <div className={styles.skills}>
+            <div className={styles.singleSkill}>
+              <Image
+                className={styles.skillIcon}
+                src={blockchain}
+                alt="Finance skill"
+                width="32"
+              />
+              Blockchain
+            </div>
+          </div>
+        </div>
+        <div className={styles.questActions}>
+          <a
+            href="#"
+            className={classNames(
+              "button",
+              "button-accent",
+              styles.questActionButton
+            )}
+          >
+            Start quest
+          </a>
+        </div>
+      </div>
+      <div className={styles.quest}>
+        <div className={styles.bgImageWrapper}>
+          <Image
+            className={styles.questImage}
+            src={quest3}
+            alt="Blockchain basics"
+          />
+        </div>
+        <div className={styles.questAbout}>
+          <h3>DAOs</h3>
+          <p>Dscription of this quest</p>
+          <div className={styles.skills}>
+            <div className={styles.singleSkill}>
+              <Image
+                className={styles.skillIcon}
+                src={daos}
+                alt="Finance skill"
+                width="32"
+              />
+              DAO
+            </div>
+            <div className={styles.singleSkill}>
+              <Image
+                className={styles.skillIcon}
+                src={finance}
+                alt="Tokens skill"
+                width="32"
+              />
+              Finance basics
+            </div>
+          </div>
+        </div>
+        <div className={styles.questActions}>
+          <a
+            href="#"
+            className={classNames(
+              "button",
+              "button-accent",
+              styles.questActionButton
+            )}
+          >
+            Start quest
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
