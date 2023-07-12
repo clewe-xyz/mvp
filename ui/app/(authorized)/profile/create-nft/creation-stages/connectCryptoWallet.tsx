6105863c678dd1e68649abdbdb597496628c5204
needpower <artemlyubchuk@mail.ru>
2023-07-12 18:26:05 +0300

    Add prod preparation materials for quests

--- ui/app/(authorized)/profile/create-nft/creation-stages/connectCryptoWallet.tsx
+++ ui/app/(authorized)/profile/create-nft/creation-stages/connectCryptoWallet.tsx
@@ -1,27 +1,16 @@
 import detectEthereumProvider from "@metamask/detect-provider";
+import { initWeb3 } from "./initWeb3";
 
 export async function connectMetaMaskWallet() {
-  let provider = await detectEthereumProvider();
-  if (!provider) {
-    throw new Error("Please install MetaMask browser extension");
-  }
-  if (typeof window !== "undefined") {
-    if (provider !== window.ethereum) {
-      throw new Error(
-        "Seems like MetaMask is not your primary provider. Switch into MetaMask to proceed"
-      );
-    }
-    try {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      return accounts[0];
-    } catch (error: any) {
-      if (error.code === 4001) {
-        throw new Error("Please accept connection to MetaMask by CleWe");
-      } else {
-        throw new Error(error);
-      }
+  try {
+    const web3 = await initWeb3();
+    const accounts = await web3.eth.requestAccounts();
+    return accounts[0];
+  } catch (error: any) {
+    if (error.code === 4001) {
+      throw new Error("Please accept connection to MetaMask by CleWe");
+    } else {
+      throw new Error(error.message);
     }
   }
 }
